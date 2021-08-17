4e354c34e6e3a487cabcb315684b0eac229264e3
Sergio Aragón <41399547+srgioar@users.noreply.github.com>
2021-08-17 21:44:11 +0200

    Update App.java

--- UD22/src/main/java/AppMain/App.java
+++ UD22/src/main/java/AppMain/App.java
@@ -4,15 +4,10 @@ import controller.Controlador;
 import model.Modelo;
 import view.Vista;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         Modelo modelo = new Modelo();
         Vista vista = new Vista();
         Controlador controlador = new Controlador(modelo, vista);
